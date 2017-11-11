84db5f7cabee3888f2cbfcb193c29fb03914e3bd
Michael Broll <michael.r.broll@gmail.com>
2017-11-10 18:28:39 -0800

    mfa: add script and readme

---
+++ README.md
@@ -0,0 +1,32 @@
+# MFA
+
+CLI wrapper for [authenticator](https://git.daplie.com/Daplie/authenticator-cli) for integration with GNU Pass.
+
+## Setup
+
+This requires using Pass to store keys given for each mfa issuer. It also relies on a specific structure for these keys in pass. They are expected to be stored in the following format:
+```
+|-- MFA
+|   |-- ISSUER
+|   |   `-- USER
+```
+For Example:
+```
+|-- MFA
+|   |-- AWS
+|   |   `-- aws.user@example.com
+|   |-- GitHub
+|   |   `-- github.user@example.com
+```
+__NOTE:__ You will want to init a new pass store with a different gpg key, protected by a different password, for the MFA directory.
+
+This also requires storing account aliases in a file under `$HOME/.mfa/aliases`. The format of this file is:
+```
+ALIAS ISSUER ACCOUNT
+...
+```
+For Example:
+```
+github GitHub github.user@example.com
+aws AWS aws.user@example.com
+```
---
+++ mfa.bash
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MFA_HOME=$HOME/.mfa
+ALIAS_FILE=$MFA_HOME/aliases
+
+USAGE="Get tokens for mfa or qr codes to sync mfa devices.
+Usage: mfa CMD ALIAS
+
+where CMD is one of:
+
+token     : Get an MFA token.
+qr        : Print a qr code to sync with mobile mfa applications like google authenticator.
+-h --help : Print this help documentation.
+
+ALIAS is a user-defined ALIAS to an issuer (GitHub, AWS, etc..), and account (typically email or username).
+Aliases are stored in $HOME/.mfa/aliases, each on their own line in the form of: \"alias_name issuer account\".
+"
+
+CMD=$1
+shift
+
+if [ "$CMD" = "" ]; then
+    echo "$USAGE"
+    exit 0
+fi
+
+ALIAS=$1
+
+if [ "$ALIAS" = "" ]; then
+    echo "no alias provided"
+    exit 1
+fi
+
+
+declare -A ALIASES
+# load aliases config
+while read -r line; do
+    KEY="$(awk '{print $1}' <(echo "$line"))"
+    VALUE="$(awk '{print $2 " " $3}' <(echo "$line"))"
+    ALIASES["$KEY"]="$VALUE"
+done <"$ALIAS_FILE"
+
+ISSUER=$(echo ${ALIASES["$ALIAS"]} | awk '{print $1}')
+ACCT=$(echo ${ALIASES["$ALIAS"]} | awk '{print $2}')
+
+if [[ "$ISSUER" = "" || "$ACCT" = "" ]]; then
+    echo "Invalid alias: \"$ALIAS\""
+    exit 1
+fi
+
+KEY="$(pass show MFA/${ISSUER}/${ACCT})"
+
+case "$CMD" in
+    token)
+    authenticator --key "$KEY" --issuer "$ISSUER" --account "$ACCT" | grep 'Token' | awk '{print $2}'
+    ;;
+    qr)
+    authenticator --key "$KEY" --issuer "$ISSUER" --account "$ACCT" --qr
+    ;;
+    -h | --help)
+    echo "$USAGE"
+    ;;
+    *)
+    echo "Unknown command: \"$CMD\""
+    echo "See mfa --help for usage"
+    exit 1
+    ;;
+esac
